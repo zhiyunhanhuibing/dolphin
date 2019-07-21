ebc16882af98b09887740713a50b8588d8003279
282691156@qq.com//你的GitHub登陆名 <282691156@qq.com //GitHub>
2019-07-21 17:17:15 +0800

    客户端认证至eureka集群

---
+++ dolphin-eureka/src/main/java/com/zhiyun/WebSecurityConfig.java
@@ -0,0 +1,20 @@
+package com.zhiyun;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * @author 新版的security默认启用了csrf检验，要在eureka服务端那边配置security的csrf检验为false
+ */
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/actuator/**").permitAll()
+                .anyRequest()
+                .authenticated().and().httpBasic();
+    }
+}
--- dolphin-manager/src/main/java/com/zhiyun/AppRun.java
+++ dolphin-manager/src/main/java/com/zhiyun/AppRun.java
@@ -14,7 +14,8 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 @EnableAsync//EnableAsync注解的意思是可以异步执行，就是开启多线程的意思。可以标注在方法、类上。为了让@Async注解能够生效，还需要在Spring Boot的主程序中配置@EnableAsync
 @SpringBootApplication
 @EnableTransactionManagement//@EnableTransactionManagement 开启事务支持
-@EnableEurekaClient //启动EnableEureka客户端
+
+@EnableEurekaClient
 public class AppRun {
 
     public static void main(String[] args) {
